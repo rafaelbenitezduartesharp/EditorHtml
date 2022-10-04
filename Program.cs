e7aea0a8e030347fd35c98e4397ece90c16fa567
Rafael Benitez Duarte <rafaelbenitezdaurtesharp@gmail.com>
2022-10-03 20:54:31 -0400

    fist commit

---
+++ Program.cs
@@ -0,0 +1,15 @@
+using System;
+
+
+namespace EditorHtml
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Menu.Show();
+
+            // Console.WriteLine("rafael");
+        }
+    }
+}
